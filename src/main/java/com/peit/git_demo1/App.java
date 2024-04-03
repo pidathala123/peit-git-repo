f3079f7396907674be80d9e4431fced483c3234d
pidathala123 <165808792+pidathala123@users.noreply.github.com>
2024-04-02 20:17:16 -0700

    Update App.java

--- src/main/java/com/peit/git_demo1/App.java
+++ src/main/java/com/peit/git_demo1/App.java
@@ -17,6 +17,12 @@ public class App
 			System.out.println("welocome");
 		}
 	}
+
+	public void addedMethod()
+	{
+
+System.out.println("Hello world");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
